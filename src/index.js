812275da340d14b614a98aca58aa3eeaf7a5d13b
georgethedeveloper77 <georgekarani77@gmail.com>
2021-06-18 11:56:56 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from "react";
+import FreddyImage from "./media/Textures/CustomNight/freddy.png";
+import ReactDOM from "react-dom";
+import Controller from "./Controller";
+import "./css/Game.css";
+import * as serviceWorker from "./serviceWorker";
+import {Provider} from "react-redux";
+import store from "./store/store";
+
+const Start = () => {
+    const [Start, setStart] = useState(false);
+
+    useEffect(() => {
+        console.log(window.innerHeight > window.innerWidth);
+        if (window.innerHeight > window.innerWidth) {
+            window.alert(
+                `Para uma melhor experiência, vire seu celular para o modo de paisagem (modo deitado)
+    
+    ~ For a better experience, please rotate your phone to landscape mode`
+            );
+        }
+    }, []);
+
+    return (
+        <>
+            {!Start ? (
+                <>
+                    {localStorage.getItem("★") ? (
+                        <div
+                            style={{
+                                position: "absolute",
+                                fontSize: "50pt",
+                                color: "white",
+                                marginLeft: "2vw",
+                            }}
+                        >
+                            ★
+                        </div>
+                    ) : null}
+                    <div className="start-screen">
+                        <div>
+                            <img
+                                alt="Five Nights At Freddy's"
+                                src={FreddyImage}
+                            />
+                            <button onClick={() => setStart(true)}>
+                                READY
+                            </button>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <Controller setStart={setStart}/>
+            )}
+        </>
+    );
+};
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Start/>
+    </Provider>,
+    document.getElementById("root")
+);
+
+serviceWorker.unregister();
